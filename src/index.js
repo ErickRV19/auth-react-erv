5f266c57f97180c91f924a72e5378f807c8ec326
Erick R <erickson.998070@gmail.com>
2021-07-23 17:16:20 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Auth0Provider} from '@auth0/auth0-react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+/*erv*/
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider 
+            domain=""
+            clientId=""
+            redirectUri={window.location.origin}
+            >
+
     <App />
+
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
